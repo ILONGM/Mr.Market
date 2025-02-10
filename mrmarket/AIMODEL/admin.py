0175cd8fc05d6577df2e3dd35c042cb87e0cf5d6
mathieu <barletmathieu@gmail.com>
2025-02-11 00:06:40 +0100

    django setup2

--- mrmarket/AIMODEL/admin.py
+++ mrmarket/AIMODEL/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MarketData
+
+@admin.register(MarketData)
+class MarketDataAdmin(admin.ModelAdmin):
+    list_display = ('date', 'sp500', 'nasdaq', 'dowjones', 'crude_oil', 'gold', 'eur_usd', 'treasury_10y', 'vix', 'is_abnormal')
+    list_filter = ('is_abnormal',)
+    search_fields = ('date',)
 
 # Register your models here.
