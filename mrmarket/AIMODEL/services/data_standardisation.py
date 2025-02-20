7e7b5e0577260cbf6666c6fc84167cf8c74340b2
mathieu <barletmathieu@gmail.com>
2025-02-20 23:34:52 +0100

    function normalize features

--- mrmarket/AIMODEL/services/data_standardisation.py
+++ mrmarket/AIMODEL/services/data_standardisation.py
@@ -2,4 +2,8 @@ from AIMODEL.models import MarketData
 
 import pandas as pd
 
-def convert_to_daily_change
+#Fonction permetant de normaliser les features (à reprendre demain)
+def feature_normalize(dataset):
+    mu = np.mean(dataset, axis=0)
+    sigma = np.std(dataset, axis=0)
+    return (dataset - mu) / sigma
